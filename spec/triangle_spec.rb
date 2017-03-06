4dafa6ae07c15e3d3afd2f73b18f42df5ca8c696
Jamie Cobbett <jamie@livestax.com>
2017-03-06 22:20:55 +0000

    Group the specs for readability

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -4,31 +4,35 @@ require_relative '../lib/triangle_validator'
 RSpec.describe TriangleValidator do
   let(:validator) { described_class.new }
 
-  context "the last side is too long" do
-    it "should be invalid" do
-      sides = [5, 10, 25]
-      expect(validator.valid?(sides)).to eq(false)
+  context "invalid triangles" do
+    context "the last side is too long" do
+      it "should be invalid" do
+        sides = [5, 10, 25]
+        expect(validator.valid?(sides)).to eq(false)
+      end
+    end
+
+    context "the middle side is too long" do
+      it "should be invalid" do
+        sides = [25, 10, 5]
+        expect(validator.valid?(sides)).to eq(false)
+      end
+    end
+
+    context "the first side is too long" do
+      it "should be invalid" do
+        sides = [5, 25, 10]
+        expect(validator.valid?(sides)).to eq(false)
+      end
     end
   end
 
-  context "the middle side is too long" do
-    it "should be invalid" do
-      sides = [25, 10, 5]
-      expect(validator.valid?(sides)).to eq(false)
-    end
-  end
-
-  context "the first side is too long" do
-    it "should be invalid" do
-      sides = [5, 25, 10]
-      expect(validator.valid?(sides)).to eq(false)
-    end
-  end
-
-  context "a valid triangle" do
-    it "should be valid" do
-      sides = [5, 21, 25]
-      expect(validator.valid?(sides)).to eq(true)
+  context "valid triangles" do
+    context "a valid triangle" do
+      it "should be valid" do
+        sides = [5, 21, 25]
+        expect(validator.valid?(sides)).to eq(true)
+      end
     end
   end
 end
