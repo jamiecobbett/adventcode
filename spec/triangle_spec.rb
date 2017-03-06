e56e768d656b2a64b2a582497a9f2684182c84ab
Jamie Cobbett <jamie@livestax.com>
2017-03-06 22:15:36 +0000

    More test cases

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -4,10 +4,23 @@ require_relative '../lib/triangle_validator'
 RSpec.describe TriangleValidator do
   let(:validator) { described_class.new }
 
-  context "an invalid triangle" do
+  context "the last side is too long" do
     it "should be invalid" do
       sides = [5, 10, 25]
+      expect(validator.valid?(sides)).to eq(false)
+    end
+  end
 
+  context "the middle side is too long" do
+    it "should be invalid" do
+      sides = [25, 10, 5]
+      expect(validator.valid?(sides)).to eq(false)
+    end
+  end
+
+  context "the first side is too long" do
+    it "should be invalid" do
+      sides = [5, 25, 10]
       expect(validator.valid?(sides)).to eq(false)
     end
   end
@@ -15,7 +28,6 @@ RSpec.describe TriangleValidator do
   context "a valid triangle" do
     it "should be valid" do
       sides = [5, 21, 25]
-
       expect(validator.valid?(sides)).to eq(true)
     end
   end
